15c72cca33f79809f35e96c71a4736d5f0042a4c
khoane03 <khoatk4444@gmail.com>
2023-03-02 12:42:26 +0700

    upcode

---
+++ src/NguyenVanKhoa_App.java
@@ -0,0 +1,7 @@
+public class NguyenVanKhoa_App {
+    public static void main(String[] args) {
+     System.out.println("Nguyen Van Khoa Hoc Java  ");
+     System.out.print("2121050782");
+    }
+    
+ }
